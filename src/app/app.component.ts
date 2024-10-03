b90dc154e5a285783eefe2134d6abbee019c5fdf
Robert Morales <lsurec@demosoft.com.gt>
2024-10-03 16:09:40 -0600

    "Updated app component HTML and TS files to add file input handling and processing for entrada and expresiones files"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegexInterface } from './interfaces/regex.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  nameFileEntrada?:string;
+  nameFileExpresiones?:string;
+
+  cadenas :string[] =[];
+  expresiones : RegexInterface[] = [];
+
   
   onFileSelectedExpresion(event: any) {
     const fileInput = event.target;
     const file = fileInput.files[0];
+    this.expresiones = [];
+
+
     if (file) {
+      
+      this.nameFileExpresiones = file.name;      
+      
       const reader = new FileReader();
+      
       reader.onload = (e: any) => {
         try {
-          console.log(e.target.result);
+         
+          const fileContent = e.target.result;
+        
+          // Divide el contenido del archivo en un arreglo por cada línea
+          const lines = fileContent.split('\n');
+  
+  
+          // Procesa cada línea del archivo
+          lines.forEach((line: { split: (arg0: string) => { (): any; new(): any; map: { (arg0: (part: any) => any): [any, any]; new(): any; }; }; }) => {
+            const [name, pattern] = line.split('=').map(part => part.trim());
+            if (name && pattern) {
+              // Elimina las comillas dobles del patrón si están presentes
+              const cleanPattern = pattern.replace(/"/g, '').trim();
+              
+              // Agrega el objeto a la lista
+              this.expresiones.push({ name, pattern: cleanPattern });
+            }
+          });
+
+
+          console.log(this.expresiones);
+          
+
           // Procesa el archivo aquí
         } catch (error) {
           console.error('Error al leer el archivo', error);
@@ -29,15 +65,26 @@ export class AppComponent {
   }
 
   
-  onFileSelectedCarga(event: any) {
+  onFileSelectedEntrada(event: any) {
+
+    this.cadenas = [];
     const fileInput = event.target;
     const file = fileInput.files[0];
     if (file) {
+
+      this.nameFileEntrada = file.name;      
+
       const reader = new FileReader();
       reader.onload = (e: any) => {
         try {
-          console.log(e.target.result);
-          // Procesa el archivo aquí
+          const fileContent = e.target.result;
+        
+          // Divide el contenido del archivo en un arreglo por cada línea
+          this.cadenas = fileContent.split('\n');
+
+
+          console.log(this.cadenas);
+          
         } catch (error) {
           console.error('Error al leer el archivo', error);
         }
@@ -49,5 +96,10 @@ export class AppComponent {
     fileInput.value = ''; // Esta línea es clave para resolver tu problema
   }
   
+
+  validateStr()
+  {
+    
+  }
   
 }
---
+++ src/app/interfaces/regex.interface.ts
@@ -0,0 +1,4 @@
+export interface RegexInterface{
+    name: string,
+    pattern: string,
+}
