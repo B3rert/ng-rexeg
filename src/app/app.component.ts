91a2b1517f7b719964b79efbeefa111f306445a6
Robert Morales <lsurec@demosoft.com.gt>
2024-10-03 15:21:19 -0600

    update 12 files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-regex';
+
+  
+  onFileSelectedExpresion(event: any) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        try {
+          console.log(e.target.result);
+          // Procesa el archivo aquí
+        } catch (error) {
+          console.error('Error al leer el archivo', error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  
+    // Reinicia el input de archivo para permitir volver a seleccionar el mismo archivo
+    fileInput.value = ''; // Esta línea es clave para resolver tu problema
+  }
+
+  
+  onFileSelectedCarga(event: any) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        try {
+          console.log(e.target.result);
+          // Procesa el archivo aquí
+        } catch (error) {
+          console.error('Error al leer el archivo', error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  
+    // Reinicia el input de archivo para permitir volver a seleccionar el mismo archivo
+    fileInput.value = ''; // Esta línea es clave para resolver tu problema
+  }
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    NgbModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
